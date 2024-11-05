6edffedc9a61774a405f893ea545d30a4e3450c2
MAHajian <mohammadaminhajiandev@gmail.com>
2024-11-05 09:52:05 +0330

    Add Bounded-Work-Pooling Pattern

---
+++ Bounded-Work-Pooling/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	// Declare Variables
+	start := time.Now()
+	wg := new(sync.WaitGroup)
+	tasks := make(chan int, 100)
+	var numOfWorkers, numOfTasks int = 5, 20
+
+	// Initialize Workers & Doing Tasks By Workers
+	for i := 1; i <= numOfWorkers; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			for task := range tasks {
+				doTask(id, task)
+			}
+		}(i)
+	}
+
+	// Send Tasks to Channel
+	for i := 0; i < numOfTasks; i++ {
+		tasks <- rand.Intn(100)
+	}
+
+	// Close Channel
+	close(tasks)
+	wg.Wait()
+	// Calculate Elaspsed Time From After User
+	fmt.Println("Elapsed:", time.Since(start))
+}
+
+// Doing Task Function
+func doTask(id int, task int) {
+	fmt.Println("Goroutine #", id, "doing:", task)
+	<-time.After(time.Second)
+}
